e4d941c6d6ee0e079316246ec26c9b3be777e300
Ahmet-accenture <ahmet.yazilitas@accenture.com>
2024-06-17 11:26:42 +0200

    redis

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -8,11 +8,11 @@ builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 //builder.Services.AddDistributedMemoryCache();
 
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-options.InstanceName = "SampleInstance";
-});
+// builder.Services.AddStackExchangeRedisCache(options =>
+// {
+// options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+// options.InstanceName = "SampleInstance";
+// });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
